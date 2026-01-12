97f2dcf86392e276c5d76c549b4203d1b301363c
Aethan Matthew <me@thanmatt.me>
2026-01-12 15:30:57 +0800

    Updated strdiff

--- fish/functions/strdiff.fish
+++ fish/functions/strdiff.fish
@@ -1,3 +1,4 @@
 function strdiff
-    diff -u (echo $argv[1] | psub) (echo $argv[2] | psub) | delta
+    # :: Base is shown as "old", second arg as "new"
+    git diff --no-index --word-diff-regex=. (echo $argv[1] | psub) (echo $argv[2] | psub)
 end
