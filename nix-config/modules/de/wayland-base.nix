cd5e9575c982ef4fe050720475cb2a0c4cbc0fc0
Aethan Matthew <me@thanmatt.me>
2026-03-18 12:33:17 +0800

    disabled waybar in system md

--- nix-config/modules/de/wayland-base.nix
+++ nix-config/modules/de/wayland-base.nix
@@ -10,7 +10,7 @@
 
   programs.waybar = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false; # :: niri config handles startup via spawn-at-startup
   };
 
   # :: Symlink waybar config from dotfiles
